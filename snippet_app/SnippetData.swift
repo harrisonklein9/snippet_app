b9c140402bb767ef44e0a6301e523b48f4930337
Harrison Klein <bassismything9@mierda.local>
2017-08-20 23:49:29 -0400

    my newest version of snippet

---
+++ snippet_app/SnippetData.swift
@@ -0,0 +1,35 @@
+//
+//  SnippetData.swift
+//  snippet_app
+//
+//  Created by Harrison Klein on 8/20/17.
+//  Copyright © 2017 Harrison Klein. All rights reserved.
+//
+
+import Foundation
+
+class SnippetData {
+    
+    let type: SnippetType
+    
+    init (snippetType: SnippetType) {
+        type = snippetType
+        print ("hi snippet created")
+    }
+}
+
+class TextData: SnippetData {
+    let textData: String
+    
+    init( text: String) {
+        textData = text
+        super.init(snippetType: .text)
+        print ("Text snippet data: \(textData)")
+    }
+}
+
+
+enum SnippetType: String {
+    case text = "Text"
+    case photo = "Photo"
+}
---
+++ snippet_app/snippet_app/ViewController.swift
@@ -0,0 +1,50 @@
+//
+//  ViewController.swift
+//  snippet_app
+//
+//  Created by Harrison Klein on 8/19/17.
+//  Copyright © 2017 Harrison Klein. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var data: [SnippetData] = [SnippetData]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    @IBAction func createNewSnippet(_ sender: Any) {
+        
+        let alert = UIAlertController(title: "Select Snippet Type", message: nil, preferredStyle: .actionSheet)
+        let textAction = UIAlertAction(title: "Text", style: .default) {
+            (alert: UIAlertAction!) -> Void in
+            self.data.append(SnippetData(snippetType: .text))
+        }
+        let photoAction = UIAlertAction(title: "Photo", style: .default) {
+            (alert: UIAlertAction!) -> Void in
+            self.data.append(SnippetData(snippetType: .photo))
+        }
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        
+        alert.addAction(textAction)
+        alert.addAction(photoAction)
+        alert.addAction(cancelAction)
+        present(alert, animated: true, completion: nil)
+        
+        print("ive made it")
+        
+    }
+    
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+ 
+
+}
+
